f3dc1a360e032fbd300a06653919b385666949fc
91980 <91980@DESKTOP-AQ6SRBB>
2020-12-17 19:45:30 +0530

    update test 2 and commit

--- src/main/java/GitDEMO/GitDEMO/test2.java
+++ src/main/java/GitDEMO/GitDEMO/test2.java
@@ -5,6 +5,13 @@ public class test2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		String dk="1";
+		System.out.println("test 2");
+		
+		if(dk=="1")
+		{
+			System.out.println("pass");
+		}
 	}
 
 }
